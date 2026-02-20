a0b16c6dc1322345c7259151b1dcc3bb6076c750
Will <rwilliamdaly@gmail.com>
2026-02-20 11:45:52 -0500

    Add tests/test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+"""
+Tests for 3.11 Assignment: Maze Search Implementation
+"""
+import pytest
+from main import main
+
+
+def test_main():
+    """Test the main function."""
+    # TODO: Add your test cases here
+    pass
