9c85715b1a55d478e07dbd5c144e65ba2d89ffc6
Will <rwilliamdaly@gmail.com>
2026-02-20 11:45:55 -0500

    Add maze_solvers.py

---
+++ maze_solvers.py
@@ -0,0 +1,16 @@
+"""3.11 Assignment: Maze Search Implementation maze solver interface."""
+
+def maze_solver_one(maze):
+    """Solve the maze and return the solved maze output."""
+    return maze
+
+
+def maze_solver_two(maze):
+    """Solve the maze and return the solved maze output."""
+    return maze
+
+
+def maze_solver_three(maze):
+    """Solve the maze and return the solved maze output."""
+    return maze
+
